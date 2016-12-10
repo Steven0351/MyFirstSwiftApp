308660d30bc016700055355afd2a65c9595dbbaf
Steven Sherry <affinityforapps@gmail.com>
2016-12-09 22:11:16 -0600

    My first commit

--- SwiftApp/ViewController.swift
+++ SwiftApp/ViewController.swift
@@ -9,10 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var theLabel: UILabel!
+    
+    var tapCount = 0
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        tapCount = tapCount + 1
+       
+        if tapCount >= 10 {
+            theLabel.text = "Dude, chill the fuck out..."
+        }
+        if tapCount >= 15 {
+            theLabel.text = "Seriously, you're not doing anything..."
+        }
+        if tapCount >= 20 {
+            theLabel.text = "Now this is just embarrasing..."
+        }
+        if tapCount >= 25 {
+            theLabel.text = "Bob"
+        }
+    }
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
